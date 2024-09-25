b82ce0e2997d6f7cc57c1e90c4607d74a29ed65d
EunbeenDev <ros4312@naver.com>
2024-09-25 22:02:49 +0900

    initial commit

---
+++ src/main/java/com/bloomgroom/BloomGroomApplication.java
@@ -0,0 +1,13 @@
+package com.bloomgroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomGroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomGroomApplication.class, args);
+    }
+
+}
