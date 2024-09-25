6f52083d7f9fc4729e9fd83170e87fe8f0a5f3bb
EunbeenDev <ros4312@naver.com>
2024-09-25 22:02:49 +0900

    initial commit

---
+++ src/test/java/com/bloomgroom/BloomGroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomgroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomGroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
