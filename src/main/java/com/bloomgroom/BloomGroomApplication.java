44ab62db0900d5db3e2dbcf79e411cfa4a9385ee
EunbeenDev <ros4312@naver.com>
2024-09-26 23:24:32 +0900

    [CHORE] 초기 프로젝트 세팅

--- src/main/java/com/bloomgroom/BloomGroomApplication.java
+++ src/main/java/com/bloomgroom/BloomGroomApplication.java
@@ -1,9 +1,19 @@
 package com.bloomgroom;
 
+import com.bloomgroom.global.config.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+//
+@PropertySource(value = { "classpath:database/application-database.yml" }, factory = YamlPropertySourceFactory.class)
+//@PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
+//@PropertySource(value = { "classpath:s3/application-s3.yml" }, factory = YamlPropertySourceFactory.class)
 public class BloomGroomApplication {
 
     public static void main(String[] args) {
